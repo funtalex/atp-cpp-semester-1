59b0c695561b73a003151de3aede23ef25e24e62
funtalex <sergeechevalex1004@gmail.com>
2021-12-19 00:23:25 +0300

    remake string.h

--- Alexey_Sergeechev/String/string.h
+++ Alexey_Sergeechev/String/string.h
@@ -3,37 +3,140 @@
 class String {
 public:
   String() {}
-  String(const char* s):size(strlen(s)), str(new char[size]), capacity(size) {
+ 
+  String(const char* s) : size(strlen(s)), str(new char[size]), capacity(size) {
     memcpy(str, s, size);
   }
-  String(size_t n, char c) : size(n), str(new char[n]), capacity(n) { //Can I use size instead n?
+ 
+  String(size_t n, char c) : size(n), str(new char[n]), capacity(n) {
     memset(str, c, n);
   }
+ 
   String(const String& s) : size(s.size), str(new char[s.size]), capacity(s.size) {
     memcpy(str, s.str, size);
   }
+ 
   String(char c) :size(1), str(new char[1]), capacity(1) {
     str[0] = c;
   }
+ 
   ~String() {
     delete[] str;
   }
+ 
   String& operator=(const String& s) {
     String copy = s;
     swap(copy);
     return *this;
   }
+ 
   char& operator[](size_t index) {
     return str[index];
   }
+ 
   const char& operator[](size_t index) const {
     return str[index];
   }
+ 
   size_t length() const {
     return size;
   }
  
   void push_back(char c) {
+    AddChar(c);
+  }
+ 
+  void pop_back() {
+    if (4 * size <= capacity) {
+      String copy;
+      copy.size = size;
+      copy.capacity = capacity / 2;
+      copy.str = new char[copy.capacity];
+      memcpy(copy.str, str, size);
+      swap(copy);
+    }
+    --size;
+  }
+ 
+  char& front() {
+    return str[0];
+  }
+ 
+  const char& front() const {
+    return str[0];
+  }
+ 
+  char& back() {
+    return str[size - 1];
+  }
+ 
+  const char& back() const {
+    return str[size - 1];
+  }
+ 
+  String& operator+=(char c) {
+    AddChar(c);
+    return *this;
+  }
+ 
+  String& operator+=(const String& s) {
+    if (size + s.size > capacity) {
+      String copy;
+      copy.size = size + s.size;
+      copy.capacity = capacity + s.capacity;
+      copy.str = new char[copy.capacity];
+      memcpy(copy.str, str, size);
+      memcpy(copy.str + size, s.str, s.size);
+      swap(copy);
+    }
+    else {
+      memcpy(str + size, s.str, s.size);
+      size += s.size;
+    }
+    return *this;
+  }
+ 
+  size_t find(const String& substr) const {
+    return CommonFinder(substr);
+  }
+ 
+  size_t rfind(const String& substr) const {
+    return CommonFinder(substr, true);
+  }
+ 
+  String substr(size_t start, size_t count) const {
+    String copy;
+    copy.capacity = copy.size = count;
+    copy.str = new char[count];
+    if (start + count <= size) {
+      memcpy(copy.str, str + start, count);
+    }
+    else {
+      memcpy(copy.str, str + start, size - start);
+    }
+    return copy;
+  }
+ 
+  bool empty() const {
+    return size == 0;
+  }
+ 
+  void clear() {
+    size = 0;
+  }
+ 
+ 
+private:
+  size_t size = 0;
+  char* str = nullptr;
+  size_t capacity = 0;
+  void swap(String& copy) {
+    std::swap(copy.str, str);
+    std::swap(copy.size, size);
+    std::swap(copy.capacity, capacity);
+  }
+ 
+  String& AddChar(char c)  {
     if (size == capacity) {
       String copy;
       copy.size = size;
@@ -47,109 +150,38 @@ public:
     }
     str[size++] = c;
   }
-  void pop_back() {
-    if (4 * size <= capacity) {
-      String copy;
-      copy.size = size;
-      copy.capacity = capacity / 2;
-      copy.str = new char[copy.capacity];
-      memcpy(copy.str, str, size);
-      swap(copy);
-    }
-    --size; 
-  }
  
-  char& front() {
-    return str[0];
-  }
-  const char& front() const {
-    return str[0];
-  }
-  char& back() {
-    return str[size - 1];
-  }
-  const char& back() const {
-    return str[size - 1];
-  }
- 
-  String& operator+=(char c) {
-    (*this).push_back(c);
-    return *this;
-  }
- 
-  String& operator+=(const String& s) { 
-    if (size + s.size > capacity) {
-      String copy;
-      copy.size = size + s.size;
-      copy.capacity = capacity + s.capacity;
-      copy.str = new char[copy.capacity];
-      memcpy(copy.str, str, size);
-      memcpy(copy.str + size, s.str, s.size);
-      swap(copy);
+  size_t CommonFinder(const String& substr, bool is_reversed = false) const {
+    if (is_reversed) {
+      for (int i = size - substr.size; i >= 0; --i) {
+        bool flag = true;
+        for (size_t j = 0; j < substr.size; ++j) {
+          if (str[i + j] != substr[j]) {
+            flag = false;
+            break;
+          }
+        }
+        if (flag) {
+          return i;
+        }
+      }
+      return size;
     }
     else {
-      memcpy(str + size, s.str, s.size);
-      size += s.size;
-      capacity += s.capacity;
-    }
-    return *this;
- 
-  }
- 
-  size_t find(const String& substr) const {
-    for (size_t i = 0; i <= size - substr.size; ++i) {
-      bool flag = true;
-      for (size_t j = 0; j < substr.size; ++j) {
-        if (str[i + j] != substr[j]) {
-          flag = false;
-          break;
+      for (size_t i = 0; i <= size - substr.size; ++i) {
+        bool flag = true;
+        for (size_t j = 0; j < substr.size; ++j) {
+          if (str[i + j] != substr[j]) {
+            flag = false;
+            break;
+          }
+        }
+        if (flag) {
+          return i;
         }
       }
-      if (flag) {
-        return i;
-      }
+      return size;
     }
-    return size;
-  }
-  size_t rfind(const String& substr) const {
-    for (int i = size - substr.size; i >= 0; --i) {
-      bool flag = true;
-      for (size_t j = 0; j < substr.size; ++j) {
-        if (str[i + j] != substr[j]) {
-          flag = false;
-          break;
-        }
-      }
-      if (flag) {
-        return i;
-      }
-    }
-    return size;
-  }
-  String substr(size_t start, size_t count) const {
-    String copy;
-    copy.capacity = copy.size = count;
-    copy.str = new char[count];
-    memcpy(copy.str, str + start, count);
-    return copy;
-  }
- 
-  bool empty() const {
-    return size == 0;
-  }
-  void clear() {
-    size = 0;
-  }
- 
-  
-private: 
-  size_t size = 0;
-  char* str = nullptr;
-  size_t capacity = 0;
-  void swap(String& copy) {
-    std::swap(copy.str, str);
-    std::swap(copy.size, size);
-    std::swap(copy.capacity, capacity);
   }
 };
  
@@ -174,6 +206,7 @@ std::ostream& operator<<(std::ostream& out, const String& s) {
   }
   return out;
 }
+ 
 std::istream& operator>>(std::istream& in, String& s) {
   s.clear();
   char c;
